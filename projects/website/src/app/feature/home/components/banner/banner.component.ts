d363b0e15392d95c82a8066d78c6f0c530285cb0
BearAndrew <a122333andrew@gmail.com>
2025-05-04 01:23:23 +0800

    feat: home banner

--- projects/website/src/app/feature/home/components/banner/banner.component.ts
+++ projects/website/src/app/feature/home/components/banner/banner.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss'
 })
