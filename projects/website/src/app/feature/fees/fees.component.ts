bfe81dba0749c4492a4a725d0c8d05db32149bc2
BearAndrew <a122333andrew@gmail.com>
2025-05-17 21:34:10 +0800

    feat: progress 頁面完成

--- projects/website/src/app/feature/fees/fees.component.ts
+++ projects/website/src/app/feature/fees/fees.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-fees',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './fees.component.html',
   styleUrl: './fees.component.scss'
 })
--- projects/website/src/app/feature/progress/progress.component.ts
+++ projects/website/src/app/feature/progress/progress.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-progress',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './progress.component.html',
   styleUrl: './progress.component.scss'
 })
-export class ProgressComponent {
+export class ProgressComponent implements OnInit {
+  currentDate: string;
+  currentTime: string;
+  doctors: { department: string; name: string; number: number | null }[] = [];
 
+  constructor() {
+    const now = new Date();
+    this.currentDate = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDate()}日`;
+    this.currentTime = now.toLocaleTimeString('en', { hour: '2-digit', minute: '2-digit', hour12: true });
+    this.currentTime = this.currentTime.replace('AM', 'A.M.').replace('PM', 'P.M.');
+  }
+
+  ngOnInit(): void {
+    this.getDoctorStatus();
+  }
+
+  getDoctorStatus() {
+    const initialDoctors = [
+      { department: '心臟血管科', name: '江孟修醫師', number: 29 },
+      { department: '一般內科', name: '陳大文醫師', number: 15 },
+      // { department: '兒科', name: '李小明醫師', number: 4 },
+    ]
+
+    // 確保資料必定有三筆，若不足則補足，若超過則只取前三筆
+    this.doctors = initialDoctors.slice(0, 3);
+    while (this.doctors.length < 3) {
+      this.doctors.push({ department: '', name: '', number: null });
+    }
+  }
 }
